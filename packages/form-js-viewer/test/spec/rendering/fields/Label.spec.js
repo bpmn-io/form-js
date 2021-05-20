f4f7f1dc4c1a106b0938654544aff903afdd50cf
Philipp <philippfromme@outlook.com>
2021-05-20 16:43:28 +0200

    tests(viewer): add tests for labels

---
+++ packages/form-js-viewer/test/spec/rendering/fields/Label.spec.js
@@ -0,0 +1,103 @@
+import {
+  render
+} from '@testing-library/preact/pure';
+
+import Label from '../../../../src/rendering/fields/Label';
+
+import { createFormContainer } from '../../../TestHelper';
+
+let container;
+
+
+describe('Label', function() {
+
+  beforeEach(function() {
+    container = createFormContainer();
+  });
+
+  afterEach(function() {
+    container.remove();
+  });
+
+
+  it('should render', function() {
+
+    // when
+    const { container } = createLabel({
+      id: 'foo',
+      label: 'Foo'
+    });
+
+    // then
+    const label = container.querySelector('.fjs-form-field-label');
+
+    expect(label).to.exist;
+    expect(label.textContent).to.eql('Foo');
+  });
+
+
+  it('should render children', function() {
+
+    // when
+    const { container } = createLabel({
+      id: 'foo',
+      label: 'Foo'
+    }, <span class="foo">Foo</span>);
+
+    // then
+    const label = container.querySelector('.foo');
+
+    expect(label).to.exist;
+  });
+
+
+  it('should render asterisk if required', function() {
+
+    // when
+    const { container } = createLabel({
+      id: 'foo',
+      label: 'Foo',
+      required: true
+    });
+
+    // then
+    const label = container.querySelector('.fjs-asterix');
+
+    expect(label).to.exist;
+  });
+
+
+  it('should not render if no label', function() {
+
+    // when
+    const { container } = createLabel({
+      id: 'foo'
+    });
+
+    // then
+    const label = container.querySelector('.fjs-form-field-label');
+
+    expect(label).not.to.exist;
+  });
+
+});
+
+// helpers //////////
+
+function createLabel(options = {}, children = null) {
+  const {
+    id,
+    label,
+    required
+  } = options;
+
+  return render(
+    <Label
+      id={ id }
+      label={ label }
+      required={ required }>{ children }</Label>,
+    {
+      container: options.container || container.querySelector('.fjs-form')
+    }
+  );
+}
