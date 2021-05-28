e915f75899fa2ee056b0320f4cbcbf083d2a5682
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-05-28 14:54:34 +0200

    chore(editor): provide UMD distribution

--- packages/form-js-editor/rollup.config.js
+++ packages/form-js-editor/rollup.config.js
@@ -1,11 +1,44 @@
 import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import copy from 'rollup-plugin-copy';
-import json from '@rollup/plugin-json';
 import reactSvg from 'rollup-plugin-react-svg';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 import pkg from './package.json';
+
+
+function pgl(options = {}) {
+
+  const {
+    resolve: resolveOptions = {},
+    commonjs: commonjsOptions,
+    copy: copyOptions
+  } = options;
+
+  return [
+    alias({
+      entries: [
+        { find: 'react', replacement: 'preact/compat' },
+        { find: 'react-dom', replacement: 'preact/compat' }
+      ]
+    }),
+    reactSvg(),
+    babel({
+      babelHelpers: 'bundled',
+      plugins: [
+        [ '@babel/plugin-transform-react-jsx', {
+          'importSource': 'preact',
+          'runtime': 'automatic'
+        } ]
+      ]
+    }),
+    copyOptions && copy(copyOptions),
+    resolveOptions && resolve(resolveOptions),
+    commonjsOptions && commonjs()
+  ];
+
+}
 
 export default [
   {
@@ -22,37 +55,35 @@ export default [
         file: pkg.module
       }
     ],
-    plugins: [
-      alias({
-        entries: [
-          { find: 'react', replacement: 'preact/compat' },
-          { find: 'react-dom', replacement: 'preact/compat' }
+    plugins: pgl({
+      resolve: {
+        resolveOnly: [
+          'min-dash',
+          'mitt'
         ]
-      }),
-      reactSvg(),
-      babel({
-        babelHelpers: 'bundled',
-        plugins: [
-          [ '@babel/plugin-transform-react-jsx', {
-            'importSource': 'preact',
-            'runtime': 'automatic'
-          } ]
-        ]
-      }),
-      copy({
+      },
+      copy: {
         targets: [
           { src: 'node_modules/@bpmn-io/form-js-viewer/dist/assets/form-js.css', dest: 'dist/assets' },
           { src: 'assets/form-js-editor.css', dest: 'dist/assets' },
           { src: '../../node_modules/dragula/dist/dragula.css', dest: 'dist/assets' }
         ]
-      }),
-      json(),
-      resolve({
-        resolveOnly: [
-          'min-dash',
-          'mitt'
-        ]
-      })
-    ]
+      }
+    })
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        sourcemap: true,
+        format: 'umd',
+        file: pkg['umd:main'],
+        name: 'FormEditor'
+      }
+    ],
+    plugins: pgl({
+      resolve: {},
+      commonjs: {}
+    })
   }
 ];
