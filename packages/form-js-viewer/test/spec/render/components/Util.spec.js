59e678845f40344224d005d9809088b8aa0cb7b9
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-06-18 11:11:50 +0200

    fix: properly generate paragraphs
    Related to https://github.com/bpmn-io/form-js/issues/87

--- packages/form-js-viewer/test/spec/render/components/Util.spec.js
+++ packages/form-js-viewer/test/spec/render/components/Util.spec.js
@@ -1,5 +1,6 @@
 import {
   formFieldClasses,
+  markdownToHTML,
   safeMarkdown
 } from '../../../../src/render/components/Util';
 
@@ -25,6 +26,127 @@ describe('Util', function() {
 
       // then
       expect(classes).to.equal('fjs-form-field fjs-form-field-button fjs-has-errors');
+    });
+
+  });
+
+
+  describe('markdownToHTML', function() {
+
+    it('should generate paragraphs', function() {
+
+      // given
+      const markdown = `
+# H1
+
+__Lorem ipsum dolor__ first.
+
+Second.
+Second continued.
+
+## H2
+
+Third.
+
+1.  List
+    List continued.
+2.  Second List
+
+*   Unordered List
+    Unordered list continued
+*   Second Unordered List Item
+
+![some small image](https://via.placeholder.com/400x150)
+![ſome big image](https://via.placeholder.com/1000x150)
+      `.trim();
+
+      const expectedHTML = `
+<h1>H1</h1>
+
+<p><strong>Lorem ipsum dolor</strong> first.</p>
+
+<p>Second.
+Second continued.</p>
+
+<h2>H2</h2>
+
+<p>Third.</p>
+
+<p><ol><li>List</li></ol><pre class="code poetry"><code>List continued.</code></pre><ol><li>Second List</li></ol></p>
+
+<ul><li>Unordered List</li></ul><pre class="code poetry"><code>Unordered list continued</code></pre><ul><li>Second Unordered List Item</li></ul>
+
+<p><img src="https://via.placeholder.com/400x150" alt="some small image">
+<img src="https://via.placeholder.com/1000x150" alt="ſome big image"></p>
+      `.trim();
+
+      // when
+      const html = markdownToHTML(markdown);
+
+      // then
+      expect(html).to.eql(expectedHTML);
+    });
+
+
+    it('should not paragraph-wrap heading', function() {
+
+      // given
+      const markdown = '#Foo<span>HELLO</span>';
+
+      // when
+      const html = markdownToHTML(markdown);
+
+      // then
+      expect(html).to.equal('<h1>Foo<span>HELLO</span></h1>');
+    });
+
+
+    it('should not paragraph-wrap HTML markup', function() {
+
+      // given
+      const markdown = '<h1>HELLO</h1>';
+
+      // when
+      const html = markdownToHTML(markdown);
+
+      // then
+      expect(html).to.equal('<h1>HELLO</h1>');
+    });
+
+
+    it('should create link', function() {
+
+      // given
+      const markdown = 'at [varius](http://foo) turpis nunc eget.';
+
+      // when
+      const html = markdownToHTML(markdown);
+
+      // then
+      expect(html).to.equal('<p>at <a href="http://foo">varius</a> turpis nunc eget.</p>');
+    });
+
+
+    it('should create heading + paragraph', function() {
+
+      // given
+      const markdown = `
+# H1
+
+Do [this](http://localhost), not __that__.
+      `.trim();
+
+      const expectedHTML = `
+<h1>H1</h1>
+
+<p>Do <a href="http://localhost">this</a>, not <strong>that</strong>.</p>
+      `.trim();
+
+      // when
+      const html = markdownToHTML(markdown);
+
+      // then
+      expect(html).to.eql(expectedHTML);
     });
 
   });
