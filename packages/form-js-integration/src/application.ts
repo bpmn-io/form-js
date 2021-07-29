ef8198787ba98e92d71f47d1fb881acfade7f521
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-07-29 11:02:30 +0200

    test: verify usage in TypeScript application
    This adds the integration package as well as a small application
    that showcases usage of the API via TypeScript.
    
    As we validate the usage via `tsc` this catches errors with our 
    existing type definitions.
    
    Related to #142

---
+++ packages/form-js-integration/src/application.ts
@@ -0,0 +1,59 @@
+import {
+  createForm,
+  createFormEditor,
+  Form,
+  FormEditor,
+  schemaVersion
+} from '@bpmn-io/form-js';
+
+
+/**
+ * A TypeScript application that verifies our type
+ * definitions are stable (enough).
+ */
+export async function createApp(options: { container: Element }) {
+
+  const {
+    container
+  } = options;
+
+  await createForm({
+    schema: {},
+    data: {},
+    container
+  });
+
+  await createFormEditor({
+    schema: {},
+    container
+  });
+
+  if (schemaVersion > 1) {
+    console.log('schemaVersion > 1');
+  }
+
+  const form = new Form({ container });
+
+  await form.importSchema({});
+  await form.importSchema({}, {});
+
+  form.setProperty('readonly', true);
+
+  // eslint-disable-next-line
+  const detachedForm = new Form();
+
+  const formEditor = new FormEditor({ container });
+
+  await formEditor.importSchema({});
+
+  // eslint-disable-next-line
+  const formEditorExtraOpts = new FormEditor({
+    container,
+    foo: {
+      bar: true
+    }
+  });
+
+  // eslint-disable-next-line
+  const detachedFormEditor = new FormEditor();
+}
