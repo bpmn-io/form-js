b94d3ad13d608375a7ed4e4b01246687567106b8
Philipp <philippfromme@outlook.com>
2021-08-18 08:10:56 +0200

    feat(editor): add editor action to select form field

--- packages/form-js-editor/src/features/editor-actions/FormEditorActions.js
+++ packages/form-js-editor/src/features/editor-actions/FormEditorActions.js
@@ -15,18 +15,38 @@ export default class FormEditorActions extends EditorActions {
   }
 
   _registerDefaultActions(injector) {
-    const commandStack = injector.get('commandStack', false);
+    const commandStack = injector.get('commandStack', false),
+          formFieldRegistry = injector.get('formFieldRegistry', false),
+          selection = injector.get('selection', false);
 
     if (commandStack) {
 
       // @ts-ignore
-      this.register('undo', function() {
+      this.register('undo', () => {
         commandStack.undo();
       });
 
       // @ts-ignore
-      this.register('redo', function() {
+      this.register('redo', () => {
         commandStack.redo();
+      });
+    }
+
+    if (formFieldRegistry && selection) {
+
+      // @ts-ignore
+      this.register('selectFormField', (options = {}) => {
+        const { id } = options;
+
+        if (!id) {
+          return;
+        }
+
+        const formField = formFieldRegistry.get(id);
+
+        if (formField) {
+          selection.set(formField);
+        }
       });
     }
   }
