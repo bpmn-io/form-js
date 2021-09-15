c685ce3abda9592308166a1d94c142abcd9f3eda
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2021-09-15 15:42:18 +0200

    fix(viewer): always render label
    Prevents components wrapped in labels and the required indicator from
    disappearing.
    
    Closes https://github.com/bpmn-io/form-js/issues/168

--- packages/form-js-viewer/src/render/components/Label.js
+++ packages/form-js-viewer/src/render/components/Label.js
@@ -5,15 +5,13 @@ export default function Label(props) {
     required = false
   } = props;
 
-  if (!label) {
-    return null;
-  }
-
-  return <label for={ id } class="fjs-form-field-label">
-    { props.children }
-    { label }
-    {
-      required && <span class="fjs-asterix">*</span>
-    }
-  </label>;
+  return (
+    <label for={ id } class="fjs-form-field-label">
+      { props.children }
+      { label || '' }
+      {
+        required && <span class="fjs-asterix">*</span>
+      }
+    </label>
+  );
 }
