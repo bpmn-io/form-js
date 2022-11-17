006225946b08fff370c372f7f1a5ae30c5546ed7
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2022-11-17 12:02:27 +0100

    docs: document useCondition

--- packages/form-js-viewer/src/render/hooks/useCondition.js
+++ packages/form-js-viewer/src/render/hooks/useCondition.js
@@ -1,9 +1,12 @@
 import useService from './useService.js';
 
 /**
+ * Check if condition is met with given variables.
  *
- * @param {{ expression?: string } | undefined} condition
+ * @param {string | undefined} condition
  * @param {import('../../types').Data} data
+ *
+ * @returns {boolean} true if condition is met or no condition or condition checker exists
  */
 export function useCondition(condition, data) {
   const conditionChecker = useService('conditionChecker', false);
