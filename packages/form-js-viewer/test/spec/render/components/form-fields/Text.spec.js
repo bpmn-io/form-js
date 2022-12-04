4ae1b218cbfc4b23117672e3fca1def197d217c3
adrianmarinwork <adrianmarinwork@gmail.com>
2022-12-04 14:27:44 +0100

    chore(viewer): add h6 to the available HTML tags
    Closes #412

--- packages/form-js-viewer/test/spec/render/components/form-fields/Text.spec.js
+++ packages/form-js-viewer/test/spec/render/components/form-fields/Text.spec.js
@@ -80,7 +80,7 @@ Some _em_ **strong** [text](#text) \`code\`.
     const formField = container.querySelector('.fjs-form-field');
 
     expect(formField).to.exist;
-    expect(formField.innerHTML).to.eql(`<div class="markup"><div xmlns="http://www.w3.org/1999/xhtml"><h1>h1</h1><h2>h2</h2><h3>h3</h3><h4>h4</h4>
+    expect(formField.innerHTML).to.eql(`<div class="markup"><div xmlns="http://www.w3.org/1999/xhtml"><h1>h1</h1><h2>h2</h2><h3>h3</h3><h4>h4</h4><h5>h5</h5><h6>h6</h6>
 
 <blockquote>Blockquote</blockquote>
 
