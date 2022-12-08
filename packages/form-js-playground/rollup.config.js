7bb4461102a5535b1b3b60a8a5b9ac0e876bf92b
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-12-08 14:41:10 +0100

    feat(playground): handle tab in json editor

--- packages/form-js-playground/rollup.config.js
+++ packages/form-js-playground/rollup.config.js
@@ -57,6 +57,7 @@ export default [
       '@codemirror/lang-json',
       '@codemirror/lint',
       '@codemirror/view',
+      '@codemirror/commands',
       'codemirror',
       'classnames'
     ],
