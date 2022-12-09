54818c7eac92129a258bb361a996e9f1b17b444e
Valentin Serra <serval.core@gmail.com>
2022-12-09 09:24:42 +0100

    chore(viewer): move sanitizer util

--- packages/form-js-viewer/src/render/components/Util.js
+++ packages/form-js-viewer/src/render/components/Util.js
@@ -1,5 +1,4 @@
 import snarkdown from '@bpmn-io/snarkdown';
-import { get } from 'min-dash';
 import classNames from 'classnames';
 
 import { sanitizeHTML, sanitizeImageSource } from './Sanitizer';
@@ -55,50 +54,4 @@ export function safeMarkdown(markdown) {
  */
 export function safeImageSource(src) {
   return sanitizeImageSource(src);
-}
-
-export function sanitizeSingleSelectValue(options) {
-  const {
-    formField,
-    data,
-    value
-  } = options;
-
-  const {
-    valuesKey,
-    values
-  } = formField;
-
-  try {
-    const validValues = (valuesKey ? get(data, [ valuesKey ]) : values).map(v => v.value) || [];
-    return validValues.includes(value) ? value : null;
-  } catch (error) {
-
-    // use default value in case of formatting error
-    // TODO(@Skaiir): log a warning when this happens - https://github.com/bpmn-io/form-js/issues/289
-    return null;
-  }
-}
-
-export function sanitizeMultiSelectValue(options) {
-  const {
-    formField,
-    data,
-    value
-  } = options;
-
-  const {
-    valuesKey,
-    values
-  } = formField;
-
-  try {
-    const validValues = (valuesKey ? get(data, [ valuesKey ]) : values).map(v => v.value) || [];
-    return value.filter(v => validValues.includes(v));
-  } catch (error) {
-
-    // use default value in case of formatting error
-    // TODO(@Skaiir): log a warning when this happens - https://github.com/bpmn-io/form-js/issues/289
-    return [];
-  }
 }
