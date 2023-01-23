b8699dcd8e35e1d6b03d3f50b21f24a454d47d2c
Valentin Serra <serval.core@gmail.com>
2023-01-23 12:51:20 +0100

    fix: display `taglist` disabled state properly
    Closes #496
    
    Co-Authored-By: Vinícius Goulart <goulart.vs@gmail.com>

--- packages/form-js-viewer/test/spec/render/components/form-fields/Taglist.spec.js
+++ packages/form-js-viewer/test/spec/render/components/form-fields/Taglist.spec.js
@@ -153,7 +153,8 @@ describe('Taglist', function() {
 
     // when
     const { container } = createTaglist({
-      disabled: true
+      disabled: true,
+      value: [ 'tag1' ]
     });
 
     // then
@@ -162,6 +163,14 @@ describe('Taglist', function() {
 
     const taglist = container.querySelector('.fjs-taglist');
     expect(taglist.classList.contains('disabled')).to.be.true;
+
+    const tag = taglist.querySelector('.fjs-taglist-tag');
+    expect(tag).to.exist;
+    expect(tag.innerText).to.equal('Tag1');
+    expect(tag.classList.contains('disabled')).to.be.true;
+
+    const cross = tag.querySelector('fjs-taglist-tag-remove');
+    expect(cross).to.not.exist;
 
   });
 
