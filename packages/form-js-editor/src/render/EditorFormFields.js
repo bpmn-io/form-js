300785619cdfc27ca0e54bc0f06a26f6d7a22be9
Valentin Serra <serval.core@gmail.com>
2023-01-31 07:46:46 +0100

    feat: added EditorFormFields extension point

---
+++ packages/form-js-editor/src/render/EditorFormFields.js
@@ -0,0 +1,11 @@
+import { FormFields } from '@bpmn-io/form-js-viewer';
+import { editorFormFields } from './components/editor-form-fields/';
+
+export default class EditorFormFields extends FormFields {
+  constructor() {
+    super();
+    editorFormFields.forEach((formField) => {
+      this.register(formField.type, formField);
+    });
+  }
+}
