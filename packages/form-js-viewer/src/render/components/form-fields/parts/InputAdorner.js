66a262872b986fe1261f1930c6a2c6685e6f1125
Valentin Serra <valentin.serra@camunda.com>
2023-02-03 04:01:04 +0100

    fix: text adornment ellipsis
    Related to https://github.com/camunda/camunda-modeler/issues/3381

--- packages/form-js-viewer/src/render/components/form-fields/parts/InputAdorner.js
+++ packages/form-js-viewer/src/render/components/form-fields/parts/InputAdorner.js
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { isString } from 'min-dash';
 
 export default function InputAdorner(props) {
 
@@ -15,9 +16,9 @@ export default function InputAdorner(props) {
   const onAdornmentClick = () => inputRef && inputRef.current && inputRef.current.focus();
 
   return <div class={ classNames('fjs-input-group', { 'fjs-disabled': disabled }, { 'hasErrors': hasErrors }) } ref={ rootRef }>
-    { pre !== null && <span class="fjs-input-adornment border-right border-radius-left" onClick={ onAdornmentClick }> { pre } </span> }
+    { pre !== null && <span class="fjs-input-adornment border-right border-radius-left" onClick={ onAdornmentClick }> { isString(pre) ? <span>{ pre }</span> : pre } </span> }
     { children }
-    { post !== null && <span class="fjs-input-adornment border-left border-radius-right" onClick={ onAdornmentClick }> { post } </span> }
+    { post !== null && <span class="fjs-input-adornment border-left border-radius-right" onClick={ onAdornmentClick }> { isString(post) ? <span>{ post }</span> : post } </span> }
   </div>;
 
 }
