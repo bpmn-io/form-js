4d5ebd519cd3855eae4bb04fc82c0c685557c321
Valentin Serra <valentin.serra@camunda.com>
2023-03-28 20:07:37 +0200

    feat: implemented text component templating
    Related to https://github.com/camunda/team-hto/issues/141

--- packages/form-js-editor/src/render/components/editor-form-fields/EditorText.js
+++ packages/form-js-editor/src/render/components/editor-form-fields/EditorText.js
@@ -1,5 +1,6 @@
-import { Text, isExpression } from '@bpmn-io/form-js-viewer';
+import { Text } from '@bpmn-io/form-js-viewer';
 import { editorFormFieldClasses } from '../Util';
+import { useService } from '../../hooks';
 
 import { iconsByType } from '../icons';
 
@@ -9,15 +10,24 @@ export default function EditorText(props) {
 
   const Icon = iconsByType('text');
 
+  const templating = useService('templating');
+  const expressionLanguage = useService('expressionLanguage');
+
   if (!text) {
     return <div class={ editorFormFieldClasses(type) }>
       <div class="fjs-form-field-placeholder"><Icon viewBox="0 0 54 54" />Text view is empty</div>
     </div>;
   }
 
-  if (isExpression(text)) {
+  if (expressionLanguage.isExpression(text)) {
     return <div class={ editorFormFieldClasses(type) }>
       <div class="fjs-form-field-placeholder"><Icon viewBox="0 0 54 54" />Text view is populated by an expression</div>
+    </div>;
+  }
+
+  if (templating.isTemplate(text)) {
+    return <div class={ editorFormFieldClasses(type) }>
+      <div class="fjs-form-field-placeholder"><Icon viewBox="0 0 54 54" />Text view is templated</div>
     </div>;
   }
 
--- packages/form-js-editor/test/spec/render/components/form-fields/helper/index.js
+++ packages/form-js-editor/test/spec/render/components/form-fields/helper/index.js
@@ -1,8 +1,28 @@
+import { FormEditorContext } from '../../../../../../src/render/context';
 import { FormContext } from '@bpmn-io/form-js-viewer';
+import showdown from 'showdown';
+
+class MarkdownRenderer {
+  constructor() {
+    showdown.setFlavor('github');
+    this._converter = new showdown.Converter();
+  }
+
+  render(markdown) {
+    return this._converter.makeHtml(markdown);
+  }
+}
+
+MarkdownRenderer.$inject = [];
 
 export function WithEditorFormContext(Component, options = {}, formId = 'foo') {
 
   function getService(type, strict) {
+
+    const {
+      isExpression,
+      isTemplate,
+    } = options;
 
     if (type === 'form') {
       return {
@@ -17,17 +37,25 @@ export function WithEditorFormContext(Component, options = {}, formId = 'foo') {
           };
         }
       };
+    } else if (type === 'templating') {
+      return { isTemplate };
+    } else if (type === 'expressionLanguage') {
+      return { isExpression };
+    } else if (type === 'markdownRenderer') {
+      return new MarkdownRenderer();
     }
   }
 
-  const formContext = {
+  const formEditorContext = {
     getService,
     formId
   };
 
   return (
-    <FormContext.Provider value={ formContext }>
-      { Component }
+    <FormContext.Provider value={ formEditorContext }>
+      <FormEditorContext.Provider value={ formEditorContext }>
+        { Component }
+      </FormEditorContext.Provider>
     </FormContext.Provider>
   );
 }
