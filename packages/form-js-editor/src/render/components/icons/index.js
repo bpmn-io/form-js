dee18d1602af65498476aff222845a147653549c
Niklas Kiefer <niklas.kiefer@camunda.com>
2023-04-13 13:30:52 +0200

    chore(editor): replace delete icon
    Closes https://github.com/bpmn-io/form-js/issues/572

--- packages/form-js-editor/src/render/components/icons/index.js
+++ packages/form-js-editor/src/render/components/icons/index.js
@@ -1,7 +1,8 @@
 import CloseIcon from './Close.svg';
+import DeleteIcon from './Delete.svg';
 import DraggableIcon from './Draggable.svg';
 import SearchIcon from './Search.svg';
 
 export { iconsByType } from '@bpmn-io/form-js-viewer';
 
-export { CloseIcon, DraggableIcon, SearchIcon };
+export { CloseIcon, DeleteIcon, DraggableIcon, SearchIcon };
