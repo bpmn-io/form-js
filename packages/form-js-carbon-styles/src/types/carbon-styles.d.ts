335a02f7949ac2af0c2d74a138c7c4d13e66d46e
Vinicius Goulart <vinicius.goulart@camunda.com>
2023-05-02 16:40:50 +0200

    feat: Add TS typedefs

---
+++ packages/form-js-carbon-styles/src/types/carbon-styles.d.ts
@@ -0,0 +1,6 @@
+declare module '@bpmn-io/form-js-carbon-styles'{
+  import { FlattenSimpleInterpolation, GlobalStyleComponent, DefaultTheme } from 'styled-components';
+
+  const CARBON_STYLES: FlattenSimpleInterpolation;
+  const GlobalFormStyling: GlobalStyleComponent<{}, DefaultTheme>;
+}
