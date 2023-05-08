4991728b806135ac33f23be7dd4b33887801ec17
Vinicius Goulart <vinicius.goulart@camunda.com>
2023-05-08 18:04:21 +0200

    chore: Render an empty form on error

--- e2e/main.js
+++ e2e/main.js
@@ -5,34 +5,48 @@ import '@bpmn-io/form-js/dist/assets/form-js-playground.css';
 import './theme.scss';
 
 import { FormPlayground, FormEditor, Form } from '@bpmn-io/form-js';
+import emptySchema from './visual/fixtures/empty.json';
 
 const COMPONENTS = {
-  'editor': FormEditor,
-  'viewer': Form,
-  'playground': FormPlayground
+  editor: FormEditor,
+  viewer: Form,
+  playground: FormPlayground,
 };
 
-fetch('/form').then(response => {
+fetch('/form')
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
 
-  if (!response.ok) {
-    throw new Error('failed to fetch form');
-  }
+    return Promise.resolve({
+      data: {
+        component: 'playground',
+        schema: emptySchema,
+      },
+    });
+  })
+  .then((response) => {
+    const data = response.data;
 
-  return response.json();
+    const FormComponent = COMPONENTS[data.component || 'viewer'];
 
-}).then((response) => {
+    const form = new FormComponent({
+      container: document.querySelector('#container'),
+      schema: data.schema,
+      data: {},
+    });
 
-  const data = response.data;
-
-  const FormComponent = COMPONENTS[data.component || 'viewer'];
-
-  const form = new FormComponent({
-    container: document.querySelector('#container'),
-    schema: data.schema,
-    data: {}
+    if (data.component !== 'playground') {
+      form.importSchema(data.schema);
+    }
+  })
+  .catch(() => {
+    new FormPlayground({
+      container: document.querySelector('#container'),
+      schema: emptySchema,
+      data: {},
+    });
   });
 
-  if (data.component !== 'playground') {
-    form.importSchema(data.schema);
-  }
-});
+
