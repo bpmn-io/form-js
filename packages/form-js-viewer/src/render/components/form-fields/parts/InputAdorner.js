17308fdf68ed7fb58c9e4d7a24d162927bf1869c
Valentin Serra <valentin.serra@camunda.com>
2023-06-06 10:33:44 +0200

    fix: don't render empty strings as adorners
    related to team-hto/236

--- packages/form-js-viewer/src/render/components/form-fields/parts/InputAdorner.js
+++ packages/form-js-viewer/src/render/components/form-fields/parts/InputAdorner.js
@@ -4,8 +4,8 @@ import { isString } from 'min-dash';
 export default function InputAdorner(props) {
 
   const {
-    pre = null,
-    post = null,
+    pre,
+    post,
     rootRef,
     inputRef,
     children,
@@ -17,9 +17,9 @@ export default function InputAdorner(props) {
   const onAdornmentClick = () => inputRef && inputRef.current && inputRef.current.focus();
 
   return <div class={ classNames('fjs-input-group', { 'fjs-disabled': disabled, 'fjs-readonly': readonly }, { 'hasErrors': hasErrors }) } ref={ rootRef }>
-    { pre !== null && <span class="fjs-input-adornment border-right border-radius-left" onClick={ onAdornmentClick }> { isString(pre) ? <span class="fjs-input-adornment-text">{ pre }</span> : pre } </span> }
+    { pre && <span class="fjs-input-adornment border-right border-radius-left" onClick={ onAdornmentClick }> { isString(pre) ? <span class="fjs-input-adornment-text">{ pre }</span> : pre } </span> }
     { children }
-    { post !== null && <span class="fjs-input-adornment border-left border-radius-right" onClick={ onAdornmentClick }> { isString(post) ? <span class="fjs-input-adornment-text">{ post }</span> : post } </span> }
+    { post && <span class="fjs-input-adornment border-left border-radius-right" onClick={ onAdornmentClick }> { isString(post) ? <span class="fjs-input-adornment-text">{ post }</span> : post } </span> }
   </div>;
 
 }
