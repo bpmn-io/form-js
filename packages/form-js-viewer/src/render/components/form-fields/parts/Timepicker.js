4ccc3d859872fcaf838b5d1ef986e68c7db75611
Valentin Serra <valentin.serra@camunda.com>
2023-06-06 10:03:42 +0200

    fix: use a safe time interval
    related to team-hto/236

--- packages/form-js-viewer/src/render/components/form-fields/parts/Timepicker.js
+++ packages/form-js-viewer/src/render/components/form-fields/parts/Timepicker.js
@@ -24,9 +24,21 @@ export default function Timepicker(props) {
     setTime
   } = props;
 
+  const safeTimeInterval = useMemo(() => {
+
+    const allowedIntervals = [ 1, 5, 10, 15, 30, 60 ];
+
+    if (allowedIntervals.includes(timeInterval)) {
+      return timeInterval;
+    }
+
+    return 15;
+
+  }, [ timeInterval ]);
+
   const timeInputRef = useRef();
   const [ dropdownIsOpen, setDropdownIsOpen ] = useState(false);
-  const useDropdown = useMemo(() => timeInterval !== 1, [ timeInterval ]);
+  const useDropdown = useMemo(() => safeTimeInterval !== 1, [ safeTimeInterval ]);
 
   const [ rawValue, setRawValue ] = useState('');
 
@@ -37,14 +49,14 @@ export default function Timepicker(props) {
       return;
     }
 
-    const intervalAdjustedTime = time - (time % timeInterval);
+    const intervalAdjustedTime = time - (time % safeTimeInterval);
     setRawValue(formatTime(use24h, intervalAdjustedTime));
 
     if (intervalAdjustedTime != time) {
       setTime(intervalAdjustedTime);
     }
 
-  }, [ time, setTime, use24h, timeInterval ]);
+  }, [ time, setTime, use24h, safeTimeInterval ]);
 
   const propagateRawToMinute = useCallback((newRawValue) => {
 
@@ -66,42 +78,42 @@ export default function Timepicker(props) {
     }
 
     // Enforce the minutes to match the timeInterval
-    const correctedMinutes = minutes - (minutes % timeInterval);
+    const correctedMinutes = minutes - (minutes % safeTimeInterval);
 
     // Enforce the raw text to be formatted properly
     setRawValue(formatTime(use24h, correctedMinutes));
     setTime(correctedMinutes);
 
-  }, [ rawValue, timeInterval, use24h, setTime ]);
+  }, [ rawValue, safeTimeInterval, use24h, setTime ]);
 
   const timeOptions = useMemo(() => {
 
     const minutesInDay = 24 * 60;
-    const intervalCount = Math.floor(minutesInDay / timeInterval);
-    return [ ...Array(intervalCount).keys() ].map(intervalIndex => formatTime(use24h, intervalIndex * timeInterval));
+    const intervalCount = Math.floor(minutesInDay / safeTimeInterval);
+    return [ ...Array(intervalCount).keys() ].map(intervalIndex => formatTime(use24h, intervalIndex * safeTimeInterval));
 
-  }, [ timeInterval, use24h ]);
+  }, [ safeTimeInterval, use24h ]);
 
   const initialFocusIndex = useMemo(() => {
 
     // if there are no options, there will not be any focusing
-    if (!timeOptions || !timeInterval) return null;
+    if (!timeOptions || !safeTimeInterval) return null;
 
     // if there is a set minute value, we focus it in the dropdown
-    if (time) return time / timeInterval;
+    if (time) return time / safeTimeInterval;
 
     const cacheTime = parseInputTime(rawValue);
 
     // if there is a valid value in the input cache, we try and focus close to it
     if (cacheTime) {
-      const flooredCacheTime = cacheTime - cacheTime % timeInterval;
-      return flooredCacheTime / timeInterval;
+      const flooredCacheTime = cacheTime - cacheTime % safeTimeInterval;
+      return flooredCacheTime / safeTimeInterval;
     }
 
     // If there is no set value, simply focus the middle of the dropdown (12:00)
     return Math.floor(timeOptions.length / 2);
 
-  }, [ rawValue, time, timeInterval, timeOptions ]);
+  }, [ rawValue, time, safeTimeInterval, timeOptions ]);
 
   const onInputKeyDown = (e) => {
     switch (e.key) {
