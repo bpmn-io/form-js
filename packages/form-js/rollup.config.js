efaa882cfba918120c7c5fcf07bbb558075e9420
Valentin Serra <valentin.serra@camunda.com>
2023-09-12 13:05:11 +0200

    wip

--- packages/form-js/rollup.config.js
+++ packages/form-js/rollup.config.js
@@ -29,8 +29,7 @@ export default [
     plugins: [
       copy({
         targets: [
-          { src: '../../node_modules/@bpmn-io/form-js-viewer/dist/assets/**/*[!light].css', dest: 'dist/assets', },
-          { src: '../../node_modules/@bpmn-io/form-js-viewer/dist/assets/flatpickr/*.css', dest: 'dist/assets/flatpickr', },
+          { src: '../../node_modules/@bpmn-io/form-js-viewer/dist/assets/**/*.css', dest: 'dist/assets', },
           { src: '../../node_modules/@bpmn-io/form-js-editor/dist/assets/**/*.css', dest: 'dist/assets' },
           { src: '../../node_modules/@bpmn-io/form-js-playground/dist/assets/**/*.css', dest: 'dist/assets' },
           { src: '../../node_modules/@bpmn-io/form-js-carbon-styles/src/carbon-styles.js', dest: 'dist' }
