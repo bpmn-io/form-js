cac14e73ac1604c278df5b9984e2908532d04c07
Valentin Serra <valentin.serra@camunda.com>
2023-09-17 18:38:53 +0200

    chore: rename expression-language folder

--- packages/form-js-viewer/src/features/index.js
+++ packages/form-js-viewer/src/features/index.js
@@ -2,6 +2,6 @@ export { default as ExpressionLanguageModule } from './expression-language';
 export { default as MarkdownModule } from './markdown';
 export { default as ViewerCommandsModule } from './viewerCommands';
 
-export * from './expression-language';
+export * from './expressionLanguage';
 export * from './markdown';
 export * from './viewerCommands';
--- packages/form-js-viewer/src/util/index.js
+++ packages/form-js-viewer/src/util/index.js
@@ -1,5 +1,5 @@
-import FeelExpressionLanguage from '../features/expression-language/FeelExpressionLanguage.js';
-import FeelersTemplating from '../features/expression-language/FeelersTemplating.js';
+import FeelExpressionLanguage from '../features/expressionLanguage/FeelExpressionLanguage.js';
+import FeelersTemplating from '../features/expressionLanguage/FeelersTemplating.js';
 import FormFields from '../render/FormFields.js';
 
 import { get } from 'min-dash';
