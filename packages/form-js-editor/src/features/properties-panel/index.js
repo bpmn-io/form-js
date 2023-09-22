5ea829093359276e8ffcbcd502a996fa5e88ef35
Niklas Kiefer <niklas.kiefer@camunda.com>
2023-09-22 14:36:16 +0200

    feat(editor): support feelPopup module

--- packages/form-js-editor/src/features/properties-panel/index.js
+++ packages/form-js-editor/src/features/properties-panel/index.js
@@ -1,6 +1,11 @@
 import PropertiesPanelModule from './PropertiesPanelRenderer';
 
+import { FeelPopupModule } from '@bpmn-io/properties-panel';
+
 export default {
+  __depends__: [
+    FeelPopupModule
+  ],
   __init__: [ 'propertiesPanel' ],
   propertiesPanel: [ 'type', PropertiesPanelModule ]
 };
