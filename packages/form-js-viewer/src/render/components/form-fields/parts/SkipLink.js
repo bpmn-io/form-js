66ad56a5c108ab7a39ff59ed56cfdc12299e8790
Niklas Kiefer <niklas.kiefer@camunda.com>
2023-10-12 15:00:35 +0200

    feat(taglist): add skip to search
    This is to provide a skip link to quickly navigate to the search via keyboard.
    
    Cf. https://webaim.org/techniques/skipnav/#hidden
    
    Closes #435

---
+++ packages/form-js-viewer/src/render/components/form-fields/parts/SkipLink.js
@@ -0,0 +1,29 @@
+import classNames from 'classnames';
+
+import { useCallback } from 'preact/hooks';
+
+export default function SkipLink(props) {
+
+  const {
+    className,
+    label,
+    onSkip
+  } = props;
+
+  const onKeyDown = useCallback(event => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      onSkip();
+    }
+  }, [ onSkip ]);
+
+
+  return (
+    <a
+      href="#"
+      class={ classNames('fjs-skip-link', className) }
+      onKeyDown={ onKeyDown }
+    >{ label }</a>
+  );
+}
