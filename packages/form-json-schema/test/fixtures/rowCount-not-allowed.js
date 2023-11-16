a7d19ec1f282671a133fce7c6dd3e0e4c05fcc6a
Vinícius Goulart <vinicius.goulart@camunda.com>
2023-11-16 13:52:28 +0100

    chore: Bump `schemaVersion` to 13 (#896)
    * chore: Bump schemaVersion to 13
    
    * chore: Fix formatting
    
    Co-authored-by: Niklas Kiefer <niklas.kiefer@camunda.com>
    
    * fix: Fix schema version range on docs
    
    * chore: Remove newline
    
    * feat: Make columns and columnsExpression mutually exclusive
    
    * feat: Add custom error message
    
    ---------
    
    Co-authored-by: Niklas Kiefer <niklas.kiefer@camunda.com>

--- packages/form-json-schema/test/fixtures/rowCount-not-allowed.js
+++ packages/form-json-schema/test/fixtures/rowCount-not-allowed.js
@@ -4,22 +4,22 @@ export const form = {
     {
       type: 'text',
       text: 'text',
-      key: 'text'
+      rowCount: 10
     }
   ]
 };
 
 export const errors = [
   {
-    instancePath: '/components/0/key',
-    schemaPath: '#/properties/components/items/allOf/1/allOf/15/then/properties/key/false schema',
+    instancePath: '/components/0/rowCount',
+    schemaPath: '#/properties/components/items/allOf/1/allOf/16/then/properties/rowCount/false schema',
     keyword: 'false schema',
     params: {},
     message: 'boolean schema is false'
   },
   {
     instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/1/allOf/15/if',
+    schemaPath: '#/properties/components/items/allOf/1/allOf/16/if',
     keyword: 'if',
     params: { failingKeyword: 'then' },
     message: 'must match "then" schema'
