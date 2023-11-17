546bb335dcf4dc60ad8455466f2bc27e565eb9fc
Niklas Kiefer <niklas.kiefer@camunda.com>
2023-11-17 09:41:15 +0100

    feat(schema): support iFrame component

---
+++ packages/form-json-schema/test/fixtures/iframe.js
@@ -0,0 +1,13 @@
+export const form = {
+  type: 'default',
+  'components': [
+    {
+      'type': 'iframe',
+      'label': 'The bpmn-io web page',
+      'url': 'https://bpmn.io/',
+      'height': 400
+    }
+  ]
+};
+
+export const errors = null;
