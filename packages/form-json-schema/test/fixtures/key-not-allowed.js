dca8839228a2049d9dda922d56f4ccf5756d4eb4
Valentin Serra <valentin.serra@camunda.com>
2023-12-12 17:44:09 +0100

    chore(schema): adjust test indexes
    Related to #808

--- packages/form-json-schema/test/fixtures/key-not-allowed.js
+++ packages/form-json-schema/test/fixtures/key-not-allowed.js
@@ -12,14 +12,14 @@ export const form = {
 export const errors = [
   {
     instancePath: '/components/0/key',
-    schemaPath: '#/properties/components/items/allOf/1/allOf/15/then/properties/key/false schema',
+    schemaPath: '#/properties/components/items/allOf/1/allOf/16/then/properties/key/false schema',
     keyword: 'false schema',
     params: {},
     message: 'boolean schema is false'
   },
   {
     instancePath: '/components/0',
-    schemaPath: '#/properties/components/items/allOf/1/allOf/15/if',
+    schemaPath: '#/properties/components/items/allOf/1/allOf/16/if',
     keyword: 'if',
     params: { failingKeyword: 'then' },
     message: 'must match "then" schema'
