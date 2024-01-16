42f4a88a2e67a7aeb9792e1b37c2c0aa473037f1
Valentin Serra <valentin.serra@camunda.com>
2024-01-16 06:29:37 +0100

    fix: show iframe title in the editor
    Closes #990

--- packages/form-js-editor/src/render/components/editor-form-fields/EditorIFrame.js
+++ packages/form-js-editor/src/render/components/editor-form-fields/EditorIFrame.js
@@ -1,17 +1,21 @@
 import {
   iconsByType,
-  IFrame
+  IFrame,
+  Label
 } from '@bpmn-io/form-js-viewer';
 
 import { editorFormFieldClasses } from '../Util';
 
 export function EditorIFrame(props) {
-  const { field } = props;
+  const { field, domId } = props;
+
+  const { label } = field;
 
   const Icon = iconsByType(field.type);
 
   return <div class={ editorFormFieldClasses(field.type) }>
-    <div class="fjs-iframe-placeholder">
+    <Label id={ domId } label={ label } />
+    <div class="fjs-iframe-placeholder" id={ domId }>
       <p class="fjs-iframe-placeholder-text"><Icon width="32" height="24" viewBox="0 0 56 56" />iFrame</p>
     </div>
   </div>;
