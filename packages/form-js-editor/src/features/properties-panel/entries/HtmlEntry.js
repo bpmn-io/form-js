a454c422b7c482317944b59df37ae92716c339a9
Vinicius Goulart <vinicius.goulart@camunda.com>
2024-02-14 11:32:52 +0100

    fix: Fix HTML component link

--- packages/form-js-editor/src/features/properties-panel/entries/HtmlEntry.js
+++ packages/form-js-editor/src/features/properties-panel/entries/HtmlEntry.js
@@ -63,7 +63,7 @@ function Content(props) {
 
 // helpers //////////
 
-const description = <>Supports HTML, styling, and templating. Styles are automatically scoped to the HTML component. <a href="https://docs.camunda.io/docs/components/modeler/forms/form-element-library/forms-element-library-html/" target="_blank">Learn more</a></>;
+const description = <>Supports HTML, styling, and templating. Styles are automatically scoped to the HTML component. <a href="https://docs.camunda.io/docs/next/components/modeler/forms/form-element-library/forms-element-library-html/" target="_blank">Learn more</a></>;
 
 /**
   * @param {string|void} value
