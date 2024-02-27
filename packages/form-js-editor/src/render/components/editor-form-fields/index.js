55a2138d8043f24e34f455a280de5e2ad222c396
Valentin Serra <valentin.serra@camunda.com>
2024-02-27 06:58:22 +0100

    feat: expression field editor component
    Related to #1073

--- packages/form-js-editor/src/render/components/editor-form-fields/index.js
+++ packages/form-js-editor/src/render/components/editor-form-fields/index.js
@@ -2,10 +2,12 @@ import { EditorIFrame } from './EditorIFrame';
 import { EditorText } from './EditorText';
 import { EditorHtml } from './EditorHtml';
 import { EditorTable } from './EditorTable';
+import { EditorExpressionField } from './EditorExpressionField';
 
 export const editorFormFields = [
   EditorIFrame,
   EditorText,
   EditorHtml,
-  EditorTable
+  EditorTable,
+  EditorExpressionField
 ];
