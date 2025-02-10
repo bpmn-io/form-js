192ef0441508cad5d0fafd1a51d0767f8dffa07b
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-02-10 20:25:28 +0100

    fix: form input autocomplete

--- packages/form-js-playground/src/components/autocompletion/index.js
+++ packages/form-js-playground/src/components/autocompletion/index.js
@@ -11,22 +11,120 @@ export function autocompletionExtension() {
   ];
 }
 
+/**
+ * @param {import('@codemirror/autocomplete').CompletionContext} context
+ */
 function completions(context) {
   const variables = context.state.facet(variablesFacet)[0];
+  /** @type {import('@codemirror/autocomplete').Completion[]} */
+  const objectOptions = variables.map((label) => ({
+    displayLabel: `"${label}"`,
+    label: `"${label}": `,
+    type: 'variable',
+    apply: (view, completion, from, to) => {
+      const doc = view.state.doc;
+      const beforeChar = doc.sliceString(from - 1, from);
+      const line = doc.lineAt(from);
+      const indentation = /^\s*/.exec(line.text)[0];
+      const baseInsert = completion.label;
 
-  const options = variables.map((v) => ({
-    label: v,
+      if (beforeChar === '{') {
+        const insert = `\n  ${indentation}${baseInsert}\n`;
+        view.dispatch({
+          changes: {
+            from,
+            to,
+            insert,
+          },
+          selection: {
+            anchor: from + insert.length - 1,
+          },
+        });
+      } else if (beforeChar === ',') {
+        const insert = `\n${indentation}${baseInsert}`;
+        view.dispatch({
+          changes: {
+            from,
+            to,
+            insert,
+          },
+          selection: {
+            anchor: from + insert.length,
+          },
+        });
+      } else {
+        view.dispatch({
+          changes: {
+            from,
+            to,
+            insert: baseInsert,
+          },
+          selection: {
+            anchor: from + baseInsert.length,
+          },
+        });
+      }
+    },
+  }));
+  /** @type {import('@codemirror/autocomplete').Completion[]} */
+  const propertyNameOptions = variables.map((label) => ({
+    label,
     type: 'variable',
   }));
-
+  /** @type {import('@codemirror/autocomplete').Completion[]} */
+  const propertyValueOptions = [
+    {
+      label: 'true',
+      type: 'constant keyword',
+      boost: 3,
+    },
+    {
+      label: 'false',
+      type: 'constant keyword',
+      boost: 2,
+    },
+    {
+      label: 'null',
+      type: 'constant keyword',
+      boost: 1,
+    },
+    {
+      displayLabel: '[ .. ]',
+      label: '[  ]',
+      apply: (view, completion, from, to) => {
+        view.dispatch({
+          changes: { from, to, insert: completion.label },
+          selection: { anchor: from + 2 },
+        });
+      },
+    },
+    {
+      displayLabel: '{ .. }',
+      label: '{  }',
+      apply: (view, completion, from, to) => {
+        view.dispatch({
+          changes: { from, to, insert: completion.label },
+          selection: { anchor: from + 2 },
+        });
+      },
+    },
+  ];
+  let finalOptions = [];
   let nodeBefore = syntaxTree(context.state).resolve(context.pos, -1);
+  let word = context.matchBefore(/\w*/);
 
-  // handle inside property name as explicit call
-  if (nodeBefore.type.name === 'PropertyName') {
-    context.explicit = true;
+  if (['Object', '{'].includes(nodeBefore.type.name)) {
+    finalOptions = objectOptions;
   }
 
-  let word = context.matchBefore(/\w*/);
+  if (nodeBefore.type.name === 'PropertyName') {
+    context.explicit = true;
+    finalOptions = propertyNameOptions;
+  }
+
+  if (['Property', '[', 'Array'].includes(nodeBefore.type.name)) {
+    finalOptions = propertyValueOptions;
+  }
 
   if (word.from == word.to && !context.explicit) {
     return null;
@@ -34,6 +132,6 @@ function completions(context) {
 
   return {
     from: word.from,
-    options,
+    options: finalOptions,
   };
 }
