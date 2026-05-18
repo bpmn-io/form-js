4fc0a5779abe83366b02a95242c76db7f6b9a1b9
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2026-05-18 11:39:17 +0200

    chore: move executablePath inside karma function and make async

--- packages/form-js-viewer/karma.conf.js
+++ packages/form-js-viewer/karma.conf.js
@@ -7,7 +7,6 @@ const browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(',');
 const singleStart = process.env.SINGLE_START;
 
 // use puppeteer provided Chrome for testing
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 const suite = coverage ? 'test/coverageBundle.js' : 'test/testBundle.js';
 
